c3eb61b44a6348c08243c53e58d57d463b5ee7b6
Casa <familiar@iMac-de-Casa.local>
2024-11-13 23:09:35 -0300

    test: testing automatic deployment

--- scripts/build-pug.js
+++ scripts/build-pug.js
@@ -1,17 +1,17 @@
-'use strict';
-const upath = require('upath');
-const sh = require('shelljs');
-const renderPug = require('./render-pug');
+"use strict";
+const upath = require("upath");
+const sh = require("shelljs");
+const renderPug = require("./render-pug");
 
-const srcPath = upath.resolve(upath.dirname(__filename), '../src');
+const srcPath = upath.resolve(upath.dirname(__filename), "../src");
 
 // Explicitly process both English and Spanish templates
 function processTemplates() {
-    const englishTemplate = upath.resolve(srcPath, 'pug/index.pug');
-    const spanishTemplate = upath.resolve(srcPath, 'pug/index-es.pug');
+  const englishTemplate = upath.resolve(srcPath, "pug/index.pug");
+  const spanishTemplate = upath.resolve(srcPath, "pug/index-es.pug");
 
-    renderPug(englishTemplate, 'dist/index.html');
-    renderPug(spanishTemplate, 'dist/index-es.html');
+  renderPug(englishTemplate, "dist/index.html");
+  renderPug(spanishTemplate, "dist/index-es.html");
 }
 
 processTemplates();
